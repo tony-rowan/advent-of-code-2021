121a7653dcf7999730e537274bc1929f226758f7
Tony Rowan <trowan812@gmail.com>
2021-12-08 15:15:50 +0000

    Day 1, Part 1, Correct - 2272262

---
+++ day-02/part-1/solve.exs
@@ -0,0 +1,26 @@
+parse_instruction = fn (instruction) ->
+  [word, value] = String.split(instruction)
+  %{word: word, value: String.to_integer(value)}
+end
+
+reduce_instruction = fn (ins, acc) ->
+  case ins do
+    %{word: "forward", value: value} -> 
+      [horizontal: acc[:horizontal] + value, depth: acc[:depth]]
+    %{word: "up", value: value} ->
+      [horizontal: acc[:horizontal], depth: acc[:depth] - value]
+    %{word: "down", value: value} ->
+      [horizontal: acc[:horizontal], depth: acc[:depth] + value]
+  end
+end
+
+{[input: input], _, _} = OptionParser.parse(System.argv(), strict: [input: :string])
+
+answer = File.stream!(input)
+  |> Stream.map(&String.trim/1)
+  |> Stream.map(parse_instruction)
+  |> Enum.reduce([horizontal: 0, depth: 0], reduce_instruction)
+
+IO.inspect(answer)
+IO.inspect(answer[:horizontal] * answer[:depth])
+
